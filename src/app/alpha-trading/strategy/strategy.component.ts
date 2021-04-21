578fe0962e57d0ce925e2a11202704af37ebb3ca
nileshkrishna <81105528+nileshkrishna@users.noreply.github.com>
2021-04-21 06:53:23 +0530

    added strategy

---
+++ src/app/alpha-trading/strategy/strategy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alpha-trading',
+  templateUrl: './alpha-trading.component.html',
+  styleUrls: ['./alpha-trading.component.scss']
+})
+export class AlphaTradingComponent {
+
+}
